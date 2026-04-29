d84110a28c914664fa289826eab6ec179aea9080
Bhavika Daga <48dagabhavika@gmail.com>
2026-04-29 10:52:31 +0530

    Modified userController and index

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const authRoutes = require('./routes/auth.js');
-require('dotenv').config();
 const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors());
+
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'https://localhost:3000',
+    credentials: true
+}));
 app.use(express.json());
 
+// sessions
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninstalized: false,
+    store: MongoStore.create({mongourl: process.env.MONGODB_URI}),
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 14*24*60*60*1000
+    }
+}));
+
 app.use('/auth', authRoutes);
-app.get('/', (req, res) => res.send("Heyy"));
+app.get('/', (req, res) => res.send("Auth server running"));
 
 const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("MongoDB connected"))
+.then(() => {
+    console.log("MongoDB connected");
+    app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`);
+    });
+})
 .catch(err => console.log("Error detected", err));
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
--- model/user.js
+++ model/user.js
@@ -1,9 +1,57 @@
 const mongoose = require('mongoose');
+const { date, lowercase } = require('zod');
+const { da } = require('zod/locales');
+const { required } = require('zod/mini');
 
-const userSchema = mongoose.Schema({
-    name: {type:String, required: true},
-    email: {type:String, required: true, unique: true},
-    password: {type:String, required: true},
-}, {timestamps: true})
+const sessionSchema = new mongoose.Schema({
+    sessionId: {type: String},
+    device: {type: String},
+    ip: {type: String},
+    createdAt: {type: Date, default: Date.now},
+    lastActive: {type: Date, default: Date.now},
+}, {_id: false});
 
-module.exports = mongoose.model('User', userSchema);
+const userSchmea = new mongoose.Schema({
+    name: {type: String, require: true},
+    email: {type: String, required: true, unique: true, lowercase: true},
+    password: {type: String},
+
+    // OAuth
+    providers: {
+        google: {
+            id: {type: String},
+            picture: {type: String},
+        },
+        github: {
+            id: {type: String},
+            picture: {type: String},
+        }
+    },
+
+    // Role base access
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+
+    // email verificationn
+    isEmailVerified: {type: Boolean, default: false},
+    emailVerifyToken: {type: String},
+    emailVerifyExpires: {type: Date},
+
+    passwordResetToken: {type: String},
+    passwordResetExpires: {type: String},
+
+    lastLogin: {type: String},
+    activeSessions: [sessionSchema],
+
+}, {timestamps: true});
+
+userSchmea.methods.toJSON = function () {
+    const obj = this.toObject(); //plain json data
+    delete obj.password;
+    delete obj.emailVerifyToken;
+    delete obj.passwordResetToken;
+    return obj; 
+}
